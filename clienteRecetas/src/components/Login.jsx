995042e3a3083e47efc0f323f166b153330bc8c0
Aureum001 <antonioayr.94@gmail.com>
2024-12-23 15:54:39 +0100

    Fix login validación

--- clienteRecetas/src/components/Login.jsx
+++ clienteRecetas/src/components/Login.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
-import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import { Form, Button, Container, Row, Col, Alert } from "react-bootstrap";
 import './Login.css';
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (!email || !password) {
+      setError('Rellena el usuario y la contraseña.');
+      return;
+    }
     console.log("Login attempted with:", { email, password });
     // Here you would typically send a request to your server
   };
@@ -17,22 +22,23 @@ function Login() {
       <Row className="justify-content-md-center mt-5">
         <Col xs={12} md={12}>
           <h2 className="text-center mb-4">Login</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
+              <Form.Label>Email</Form.Label>
               <Form.Control
                 type="email"
-                placeholder="Enter email"
+                placeholder="Introduce tu email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
+              <Form.Label>Contraseña</Form.Label>
               <Form.Control
                 type="password"
-                placeholder="Password"
+                placeholder="Introduce tu contraseña"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
