d966b0dd7c3d0c286e3b138e2c0e60d403163124
Aureum001 <antonioayr.94@gmail.com>
2024-12-23 16:01:14 +0100

    Fix validación v2

--- clienteRecetas/src/components/Login.jsx
+++ clienteRecetas/src/components/Login.jsx
@@ -1,20 +1,33 @@
 import React, { useState } from "react";
 import { Form, Button, Container, Row, Col, Alert } from "react-bootstrap";
-import './Login.css';
+import "./Login.css";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState('');
+  const [errors, setErrors] = useState("");
+
+  const validateForm = () => {
+    const newErrors = {};
+    if (!email) newErrors.email = "Email requerido";
+    else if (!/\S+@\S+\.\S+/.test(email)) newErrors.email = "Email invalido";
+    if (!password) newErrors.password = "contraseña requerida";
+    else if (password.length < 6)
+      newErrors.password =
+        "La contraseña tiene que tener al menos 6 carácteres";
+    return newErrors;
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (!email || !password) {
-      setError('Rellena el usuario y la contraseña.');
-      return;
+    const formErrors = validateForm();
+    if (Object.keys(formErrors).length > 0) {
+      setErrors(formErrors);
+    } else {
+      setErrors({});
+      console.log("Login attempted with:", { email, password });
+      // Here you would typically send a request to your server
     }
-    console.log("Login attempted with:", { email, password });
-    // Here you would typically send a request to your server
   };
 
   return (
@@ -22,7 +35,6 @@ function Login() {
       <Row className="justify-content-md-center mt-5">
         <Col xs={12} md={12}>
           <h2 className="text-center mb-4">Login</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email</Form.Label>
@@ -31,7 +43,11 @@ function Login() {
                 placeholder="Introduce tu email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                isInvalid={!!errors.email}
               />
+              <Form.Control.Feedback type="invalid">
+                {errors.email}
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -41,7 +57,11 @@ function Login() {
                 placeholder="Introduce tu contraseña"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                isInvalid={!!errors.password}
               />
+              <Form.Control.Feedback type="invalid">
+                {errors.password}
+              </Form.Control.Feedback>
             </Form.Group>
 
             <Button variant="primary" type="submit" className="w-100">
