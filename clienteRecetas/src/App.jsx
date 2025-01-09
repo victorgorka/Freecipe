116ed888a224d77c95a2ef9560983eeeaa4a2905
Victor Gorka De Prada Arnedo <vde-prad@student.42malaga.com>
2025-01-09 18:14:51 +0100

    Add Home component to src in ClienteRecetas

--- clienteRecetas/src/App.jsx
+++ clienteRecetas/src/App.jsx
@@ -4,11 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login';
+import Home from './components/Home';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Login />
+    <Home />
   )
 }
 
---
+++ clienteRecetas/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "./Home.css";
+
+
+function Home() {
+	  return (
+	<Container>
+	
+	</Container>
+  );
+}
+
+export default Home;
