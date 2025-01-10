766bf62affd207717dd6bf220409cf1f4873d6e4
Victor Gorka De Prada Arnedo <vde-prad@student.42malaga.com>
2025-01-10 17:12:13 +0100

    Add checkbox to select types of receips

--- clienteRecetas/src/components/home/SearchContainer.jsx
+++ clienteRecetas/src/components/home/SearchContainer.jsx
@@ -9,9 +9,20 @@ function SearchContainer() {
 				<Row>
 					<Col>
 						<div className="search-container">
-							<h1>Encuentra la receta perfecta</h1>
 							<form>
-								<input type="text" placeholder="Buscar receta" />
+								<div className="checkbox-container">
+									<input type="checkbox" id="vegan" name="vegan" value="vegan" />
+									<label htmlFor="vegan">Op. Vegana</label>
+								</div>
+								<div className="checkbox-container">
+									<input type="checkbox" id="celiaca" name="celiaca" value="celiaca" />
+									<label htmlFor="vegan">Op. Celiaca</label>
+								</div>
+								<div className="checkbox-container">
+									<input type="checkbox" id="sal" name="sal" value="sal" />
+									<label htmlFor="vegan">Op. Sin Sal</label>
+								</div>
+								<input type="text" placeholder="Buscar ingredientes" />
 								<button type="submit">Buscar</button>
 							</form>
 						</div>
