df80f13e3ee9f273db819efa3590dc4f93208e04
unknown <correoDavidPJ@gmail.com>
2025-01-13 19:41:07 +0100

    Casi

--- clienteRecetas/src/components/home/RecipeCard.jsx
+++ clienteRecetas/src/components/home/RecipeCard.jsx
@@ -2,15 +2,13 @@ import './RecipeCard.css';
 
 const RecipeCard = () => {
   return (
-    <div className="card" style={{ width: '18rem' }}>
-      <img src="https://via.placeholder.com/150" className="card-img-top" alt="..." />
-      <div className="card-body">
-      <img src="https://via.placeholder.com/150" className="image" alt="imagen"/>
-        <div className="text-container">
+    <div className="card" >
+    <img src="https://via.placeholder.com/150" className="image" alt="..." />
+     <div className="text-container">
           <h5 className="title">Spaghetti</h5>
           <p className="calories"> 157Cal - 100gr.</p>
           <a href="#" className="button">Ver receta</a>
-        </div>
+        
       </div>
     </div>
   );
--- clienteRecetas/src/components/home/SearchResults.jsx
+++ clienteRecetas/src/components/home/SearchResults.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "./SearchResults.css";
 import RecipeCard from "./RecipeCard";
 
 function SearchResults() {
+	const [numCards, setNumCards] = useState(4)
+
+	function getNext() {
+		setNumCards(numCards + 4);
+	}
+
 	return (
 		<div>
 			<Container>
 				<Row>
-					<Col>
-						<RecipeCard></RecipeCard>
+					<Col className="Col2">
+						{[...Array(numCards).keys()].map(index => (
+							<RecipeCard key={index}></RecipeCard>
+						))}
 					</Col>
+				</Row>
+				<Row>
+					{numCards < 20 &&
+					<button className="button2" onClick={getNext}>VER +</button>}
 				</Row>
 			</Container>
 		</div>
