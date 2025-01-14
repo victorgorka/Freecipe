4a7b2652565c5453772dd2b7e8bdb4b0a853aa8b
Aureum001 <antonioayr.94@gmail.com>
2025-01-14 19:25:39 +0100

    Post funciona ahora, cambios en CrudReceta

--- servidorRecetas/src/main/java/com/example/servidorRecetas/services/CrudReceta.java
+++ servidorRecetas/src/main/java/com/example/servidorRecetas/services/CrudReceta.java
@@ -1,7 +1,10 @@
 package com.example.servidorRecetas.services;
 
+import com.example.servidorRecetas.model.Ingredient;
 import com.example.servidorRecetas.model.Recipe;
+import com.example.servidorRecetas.repository.IngredientRepository;
 import com.example.servidorRecetas.repository.RecipeRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,9 @@ public class CrudReceta {
     @Autowired
     private RecipeRepository recipeRepository;
 
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
     public List<Recipe> getAllRecipes() {
         return recipeRepository.findAll();
     }
@@ -22,7 +28,23 @@ public class CrudReceta {
         return recipeRepository.findById(id);
     }
 
+    @Transactional
     public Recipe createRecipe(Recipe recipe) {
+
+        for (int i = 0; i < recipe.getIngredients().size(); i++) {
+            Ingredient ingredient = recipe.getIngredients().get(i);
+
+            // Check if the ingredient already exists in the database by its name
+            Optional<Ingredient> existingIngredient = ingredientRepository.findByName(ingredient.getName());
+
+            if (existingIngredient.isPresent()) {
+                // If the ingredient exists, use the existing entity to avoid creating a new one
+                recipe.getIngredients().set(i, existingIngredient.get());
+            } else {
+                // If the ingredient doesn't exist, save it to the database
+                ingredientRepository.save(ingredient);
+            }
+        }
 
         return recipeRepository.save(recipe);
     }
