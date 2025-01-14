57877177e54f3daac1b42dc47c3d725fbd7dc597
Victor Gorka De Prada Arnedo <vde-prad@student.42malaga.com>
2025-01-14 16:30:32 +0100

    fixing width of SearchComponent; removing grid inside the component

--- clienteRecetas/src/components/home/SearchContainer.jsx
+++ clienteRecetas/src/components/home/SearchContainer.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container, Row, Col, Form } from "react-bootstrap";
+import {Form}  from "react-bootstrap";
 import "./SearchContainer.css";
 
 function SearchContainer() {
@@ -30,47 +30,43 @@ function SearchContainer() {
 
   return (
     <div>
-      <Container>
-        <Row>
-          <Col className="search-container">
-            <div className="search-filter">
-              <form onSubmit={handleAddIngredient}>
-                <div className="checkbox-container">
-                  <input type="checkbox" id="vegan" name="vegan" value="vegan" />
-                  <label htmlFor="vegan">Op. <strong>Vegana</strong></label>
-                </div>
-                <div className="checkbox-container">
-                  <input type="checkbox" id="celiaca" name="celiaca" value="celiaca" />
-                  <label htmlFor="celiaca">Op. <strong>Celiaca</strong></label>
-                </div>
-                <div className="checkbox-container">
-                  <input type="checkbox" id="sal" name="sal" value="sal" />
-                  <label htmlFor="sal">Op. <strong>Sin Sal</strong></label>
-                </div>
-                <Form.Label>Tiempo: {rangeValue} minutos</Form.Label>
-                <Form.Range max={120} value={rangeValue} onChange={handleRangeChange} />
-                <input
-                  type="text"
-                  placeholder="Ingresar ingredientes"
-                  value={ingredient}
-                  onChange={handleInputChange}
-                  className="custom-placeholder"
-                />
-              </form>
-              <div className="submitted-ingredients">
-                {ingredients.map((ing, index) => (
-                  <div key={index} className="ingredient-item">
-                    {ing}
-                    <button onClick={() => handleRemoveIngredient(index)}>x</button>
-                  </div>
-                ))}
-              </div>
-              <br />
-              <button type="submit">Recomendar</button>
+      <div className="search-container">
+        <div className="search-filter">
+          <form onSubmit={handleAddIngredient}>
+            <div className="checkbox-container">
+              <input type="checkbox" id="vegan" name="vegan" value="vegan" />
+              <label htmlFor="vegan">Op. <strong>Vegana</strong></label>
             </div>
-          </Col>
-        </Row>
-      </Container>
+            <div className="checkbox-container">
+              <input type="checkbox" id="celiaca" name="celiaca" value="celiaca" />
+              <label htmlFor="celiaca">Op. <strong>Celiaca</strong></label>
+            </div>
+            <div className="checkbox-container">
+              <input type="checkbox" id="sal" name="sal" value="sal" />
+              <label htmlFor="sal">Op. <strong>Sin Sal</strong></label>
+            </div>
+            <Form.Label>Tiempo: {rangeValue} minutos</Form.Label>
+            <Form.Range max={120} value={rangeValue} onChange={handleRangeChange} />
+            <input
+              type="text"
+              placeholder="Ingresar ingredientes"
+              value={ingredient}
+              onChange={handleInputChange}
+              className="custom-placeholder"
+            />
+          </form>
+          <div className="submitted-ingredients">
+            {ingredients.map((ing, index) => (
+              <div key={index} className="ingredient-item">
+                {ing}
+                <button onClick={() => handleRemoveIngredient(index)}>x</button>
+              </div>
+            ))}
+          </div>
+          <br />
+          <button type="submit">Recomendar</button>
+        </div>
+      </div>
     </div>
   );
 }
