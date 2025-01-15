3d3a5ea433ddcc7c62d15070e1925c71eff17de0
Aureum001 <antonioayr.94@gmail.com>
2025-01-15 18:33:51 +0100

    Landing page inicial

--- clienteRecetas/src/components/Home.jsx
+++ clienteRecetas/src/components/Home.jsx
@@ -3,20 +3,22 @@ import { Container, Row, Col } from "react-bootstrap";
 import "./Home.css";
 import SearchContainer from "./home/SearchContainer";
 import SearchResults from "./home/SearchResults";
+import LandingPage from "./home/LandingPage";
 
 
 function Home() {
 	return (
-	<Container>
-		<Row>
-			<Col>
-				<SearchContainer />
-			</Col>
-			<Col>
-				<SearchResults />
-			</Col>
-		</Row>
-	</Container>
+	// <Container>
+	// 	<Row>
+	// 		<Col>
+	// 			<SearchContainer />
+	// 		</Col>
+	// 		<Col>
+	// 			<SearchResults />
+	// 		</Col>
+	// 	</Row>
+	// </Container>
+	<LandingPage></LandingPage>
   );
 }
 
